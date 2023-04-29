5a597753e88b99e57e58fbade548894819b2056c
Samir <mahmoudsamir109@gmail.com>
2023-04-29 23:05:53 +0200

    Update analytics.py

--- analytics/analytics.py
+++ analytics/analytics.py
@@ -14,15 +14,15 @@ print('Waiting for the data generator...')
 sleep(20)
 print('ETL Starting...')
 
-while True:
-    try:
-        psql_engine = create_engine(
-            environ["POSTGRESQL_CS"], pool_pre_ping=True, pool_size=10)
+# while True:
+#     try:
+#         psql_engine = create_engine(
+#             environ["POSTGRESQL_CS"], pool_pre_ping=True, pool_size=10)
 
-        break
-    except OperationalError:
-        sleep(0.1)
-print('Connection to PostgresSQL successful.')
+#         break
+#     except OperationalError:
+#         sleep(0.1)
+# print('Connection to PostgresSQL successful.')
 
 # Write the solution here
 
